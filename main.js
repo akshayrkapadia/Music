e0e1d41dbcb6d1b0e8a09bd3c90b78fdd3a636e8
Akshay R. Kapadia <akshay@localhost.localdomain>
2020-08-13 09:57:31 -0400

    updated

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 function toggle(track) {
         var controls = document.getElementById("controls");
         var controlsSource = document.getElementById("controls source");
+        var playing = document.getElementById("playing");
+        playing.innerHTML = "Now Playing: " + track;
         controlsSource.src = "./Music/" + track + ".mp3";
         controls.load();
         controls.play();
