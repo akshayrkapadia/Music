a3e97905d47f9608c41d7ecb04614143d0460d72
Akshay R. Kapadia <akshayrajankapadia@gmaill.com>
2020-08-14 14:57:40 -0400

    updated

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
-function play(track) {
-        var controls = document.getElementById("controls");
-        var controlsSource = document.getElementById("controls source");
-        var playing = document.getElementById("playing");
-        playing.innerHTML = "Now Playing: " + track;
-        controlsSource.src = "./Music/" + track + ".mp3";
-        controls.load();
-        controls.play();
+function play( track ) {
+	var controls = document.getElementById( "controls" );
+	var controlsSource = document.getElementById( "controls source" );
+	var playing = document.getElementById( "playing" );
+	playing.innerHTML = "Now Playing: " + track;
+	controlsSource.src = "./Music/" + track + ".mp3";
+	controls.load();
+	controls.play();
 }
 
 //function toggleNewRelease() {
-        //var newReleaseNav = document.getElementById("new release nav");
-        //var discographyNav = document.getElementById("discography nav");
-        //var newRelease = document.getElementById("new release");
-        //var discography = document.getElementById("discography");
-        //newReleaseNav.class = "nav-link active";
-        //discographyNav.class = "nav-link";
-        //newRelease.style.display = "block";
-        //discography.style.display = "none";
+//var newReleaseNav = document.getElementById("new release nav");
+//var discographyNav = document.getElementById("discography nav");
+//var newRelease = document.getElementById("new release");
+//var discography = document.getElementById("discography");
+//newReleaseNav.class = "nav-link active";
+//discographyNav.class = "nav-link";
+//newRelease.style.display = "block";
+//discography.style.display = "none";
 //}
 
 //function toggleDiscography() {
-        //var newReleaseNav = document.getElementById("new release nav");
-        //var discographyNav = document.getElementById("discography nav");
-        //var newRelease = document.getElementById("new release");
-        //var discography = document.getElementById("discography");
-        //newReleaseNav.class = "nav-link";
-        //discographyNav.class = "nav-link active";
-        //newRelease.style.display = "none";
-        //discography.style.display = "block";
+//var newReleaseNav = document.getElementById("new release nav");
+//var discographyNav = document.getElementById("discography nav");
+//var newRelease = document.getElementById("new release");
+//var discography = document.getElementById("discography");
+//newReleaseNav.class = "nav-link";
+//discographyNav.class = "nav-link active";
+//newRelease.style.display = "none";
+//discography.style.display = "block";
 
 //}
