fa5710fbbe9cc68f52b2094e19a1fab7841bb1e0
Akshay R. Kapadia <akshay@localhost.localdomain>
2020-08-14 09:14:56 -0400

    updated

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function toggle(track) {
+function play(track) {
         var controls = document.getElementById("controls");
         var controlsSource = document.getElementById("controls source");
         var playing = document.getElementById("playing");
@@ -7,3 +7,26 @@ function toggle(track) {
         controls.load();
         controls.play();
 }
+
+//function toggleNewRelease() {
+        //var newReleaseNav = document.getElementById("new release nav");
+        //var discographyNav = document.getElementById("discography nav");
+        //var newRelease = document.getElementById("new release");
+        //var discography = document.getElementById("discography");
+        //newReleaseNav.class = "nav-link active";
+        //discographyNav.class = "nav-link";
+        //newRelease.style.display = "block";
+        //discography.style.display = "none";
+//}
+
+//function toggleDiscography() {
+        //var newReleaseNav = document.getElementById("new release nav");
+        //var discographyNav = document.getElementById("discography nav");
+        //var newRelease = document.getElementById("new release");
+        //var discography = document.getElementById("discography");
+        //newReleaseNav.class = "nav-link";
+        //discographyNav.class = "nav-link active";
+        //newRelease.style.display = "none";
+        //discography.style.display = "block";
+
+//}
